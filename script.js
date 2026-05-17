1c37bcad59b6dae548b88faac112ccdf6f093d36
Petluri-0 <petluriprasanth8@gmail.com>
2026-05-17 17:22:45 +0530

    N:link mathematical logic to HTML elements for live display

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 console.log("Hello");
-const hoursElement = document.getElementById('hours');
-//hoursElement.innerHTML = 10
 const weddingDate = new Date(2026,5,29,2,45,0);
 
 function countdown()
@@ -14,17 +12,13 @@ function countdown()
     let hours = Math.floor((totalSeconds % (3600*24)) /3600);
     let minutes = Math.floor((totalSeconds % 3600) /60);
     let seconds = totalSeconds % 60 
-    console.log(weddingDate);
-    console.log(now);
-    console.log(totalMs);
-    console.log(totaldays);
-    console.log(hours);
-    console.log(minutes);
-    console.log(seconds);   
+
+    document.getElementById('days').innerText = totaldays;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
 
 }
 const update = setInterval(() => {
     countdown();
 },1000);
-//console.log(countdown())
-//const days = Math.floor(totalMs/ (1000*60*60*24));
