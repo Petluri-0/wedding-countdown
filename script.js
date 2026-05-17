364775e5218a13a091b052f0d133d2f6980439d5
Petluri-0 <petluriprasanth8@gmail.com>
2026-05-17 17:06:17 +0530

    N: complete mathematical logic for days, hours, minutes, and seconds

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ const hoursElement = document.getElementById('hours');
 //hoursElement.innerHTML = 10
 const weddingDate = new Date(2026,5,29,2,45,0);
 const now = new Date();
-total = weddingDate - now;
+const totalMs = weddingDate - now;
+
+let totalSeconds = Math.floor(totalMs/1000);
+
+let totaldays = Math.floor(totalSeconds/ (3600*24));
+let hours = Math.floor((totalSeconds % (3600*24)) /3600);
+let minutes = Math.floor((totalSeconds % 3600) /60);
+let seconds = totalSeconds % 60
+//const days = Math.floor(totalMs/ (1000*60*60*24));
 console.log(weddingDate);
 console.log(now);
-console.log(total)
+console.log(totalMs);
+console.log(totaldays);
+console.log(hours);
+console.log(minutes);
+console.log(seconds);
