012dd1d1da6df117457fd43bfcd3eee55f22eb96
Petluri-0 <petluriprasanth8@gmail.com>
2026-05-17 16:38:07 +0530

    N: Added countdown logic that gives output in milliseconds

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 console.log("Hello");
 const hoursElement = document.getElementById('hours');
-hoursElement.innerHTML = 10
+//hoursElement.innerHTML = 10
+const weddingDate = new Date(2026,5,29,2,45,0);
+const now = new Date();
+total = weddingDate - now;
+console.log(weddingDate);
+console.log(now);
+console.log(total)
